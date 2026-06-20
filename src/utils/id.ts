0c5c2b844f01ca56a7639869e153ea7fc1f23f52
gustavjung01 <gustavjung01@gmail.com>
2026-06-20 09:57:05 +0700

    feat: add local id utility

---
+++ src/utils/id.ts
@@ -0,0 +1,5 @@
+export function createLocalId(prefix: string) {
+  const time = Date.now().toString(36);
+  const random = Math.random().toString(36).slice(2, 8);
+  return `${prefix}-${time}-${random}`;
+}
