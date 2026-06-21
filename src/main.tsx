eae3c6fa610c288f3a72756ed94e89c9ad3b7ce7
gustavjung01 <gustavjung01@gmail.com>
2026-06-21 15:09:40 +0700

    Load POS lifecycle styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './styles/tokens.css';
 import './styles/global.css';
 import './styles/layout.css';
 import './styles/components.css';
+import './styles/posLifecycle.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
